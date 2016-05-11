86f18c65d71cee4bc3d1b0f199b7696ac253124e
Usuario <Usuario@10.232.6.31>
2016-05-11 18:26:11 +0200

    Primera modificación

--- cualquiera/src/cualquiera/Cualquiera.java
+++ cualquiera/src/cualquiera/Cualquiera.java
@@ -16,6 +16,8 @@ public class Cualquiera {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.print("Primera modificación");
     }
     
 }
